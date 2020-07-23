6baa2800adc69ba1aa9f239f072f71a95bd21d0e
Hypno7767 <33670815+Hypno7767@users.noreply.github.com>
2020-07-23 16:52:25 -0400

    Create index.js
    Sorry for the absolute sh*t aligning. That's what I get for coding in Notepad instead of installing Notepad++ or Atom. Whatever makes your heart sing.

---
+++ index.js
@@ -0,0 +1,41 @@
+const Discord = require('discord.js');
+const fs = require('fs');
+const client = new Discord.Client();
+const token = "YOUR_TOKEN_HERE"
+
+client.on('ready', () => {
+  console.log(`Logged in! My name is ${client.user.tag}!`);
+
+client.user.setStatus('Online')
+client.user.setActivity('https://bit.ly/3jCcaNo', { type: 'WATCHING'}).catch(console.error);
+
+});
+
+
+client.on('message', async message => {
+    if (message.content === '%rickroll') {
+if (!message.member.voice.channel) {
+    // idk why I added this part but okay.
+    if (!message.author.bot) {
+    message.reply('You need to join a voice channel before using this command!')
+    }
+}
+        if (message.member.voice.channel) {
+            const connection = await message.member.voice.channel.join();
+            // LET IT RIP, BABY!
+const dispatcher = connection.play('audio.mp3');
+
+dispatcher.on('start', () => {
+    console.log('Someone\'s getting rickrolled!');
+});
+// sweet sweet pain has finished. we sniped em good, boi
+dispatcher.on('finish', () => {
+    console.log('Rickroll ended successfully');
+    message.member.voice.channel.leave();
+}); 
+// because error.
+dispatcher.on('error', console.error);
+        }
+    }
+
+});
